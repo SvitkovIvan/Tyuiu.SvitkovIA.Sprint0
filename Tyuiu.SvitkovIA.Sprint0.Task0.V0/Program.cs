4cdc3876000272afcf4e1593cea878f713d5223b
Tyuiu.SvitkovIA@outlook.com <svitkovia@mail.ru>
2023-09-09 00:02:42 +0500

    Task1

--- Tyuiu.SvitkovIA.Sprint0.Task0.V0/Program.cs
+++ Tyuiu.SvitkovIA.Sprint0.Task0.V0/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Tyuiu.SvitkovIA.Sprint0.Task0.V0
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
